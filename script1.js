dfb4c044fdc0d5a814667155be69802e36b31473
Michael <124568429+16bitYoda@users.noreply.github.com>
2023-10-04 16:34:07 +1000

    Update script1.js

--- script1.js
+++ script1.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const tireLimits = new Map([
     ["385", 2],
     ["295", 11],
-    ["11R", 11],
+    ["11R", 16],
     ["315", 7],
     ["305", 8],
     ["275", 11],
